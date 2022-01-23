3face47142b0e2129469158637867ba736ab7a09
Daniel JCH <danielj.callisayah@gmail.com>
2022-01-22 23:08:19 -0400

    bundle decode extension

---
+++ Moonshot/Bundle-Decodable.swift
@@ -0,0 +1,27 @@
+//
+//  Bundle-Decodable.swift
+//  Moonshot
+//
+//  Created by Daniel Jesus Callisaya Hidalgo on 22/1/22.
+//
+
+import Foundation
+extension Bundle {
+    func decode(_ file: String) -> [String: Astronaut] {
+        guard let url = self.url(forResource: file, withExtension: nil) else {
+            fatalError("Failed to locate \(file) in bundle.")
+        }
+
+        guard let data = try? Data(contentsOf: url) else {
+            fatalError("Failed to load \(file) from bundle.")
+        }
+
+        let decoder = JSONDecoder()
+
+        guard let loaded = try? decoder.decode([String: Astronaut].self, from: data) else {
+            fatalError("Failed to decode \(file) from bundle.")
+        }
+
+        return loaded
+    }
+}
