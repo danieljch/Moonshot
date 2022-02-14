52be07c96be63d9f303ba1531f4d92b161f58d3e
Daniel JCH <danielj.callisayah@gmail.com>
2022-02-14 16:42:37 -0400

    App finished

--- Moonshot/MissionView.swift
+++ Moonshot/MissionView.swift
@@ -25,13 +25,54 @@ struct MissionView: View {
                            .padding(.top)
 
                        VStack(alignment: .leading) {
+                           Rectangle()
+                               .frame(height: 2)
+                               .foregroundColor(.lightBackground)
+                               .padding(.vertical)
                            Text("Mission Highlights")
                                .font(.title.bold())
                                .padding(.bottom, 5)
 
                            Text(mission.description)
+                           Rectangle()
+                               .frame(height: 2)
+                               .foregroundColor(.lightBackground)
+                               .padding(.vertical)
+                           Text("Crew")
+                               .font(.title.bold())
+                               .padding(.bottom, 5)
                        }
                        .padding(.horizontal)
+                       
+                       ScrollView(.horizontal, showsIndicators: false) {
+                           HStack {
+                               ForEach(crew, id: \.role) { crewMember in
+                                   NavigationLink {
+                                       AstronautView(astronaut: crewMember.astronaut)
+                                   } label: {
+                                       HStack {
+                                           Image(crewMember.astronaut.id)
+                                               .resizable()
+                                               .frame(width: 104, height: 72)
+                                               .clipShape(Capsule())
+                                               .overlay(
+                                                   Capsule()
+                                                       .strokeBorder(.white, lineWidth: 1)
+                                               )
+
+                                           VStack(alignment: .leading) {
+                                               Text(crewMember.astronaut.name)
+                                                   .foregroundColor(.white)
+                                                   .font(.headline)
+                                               Text(crewMember.role)
+                                                   .foregroundColor(.secondary)
+                                           }
+                                       }
+                                       .padding(.horizontal)
+                                   }
+                               }
+                           }
+                       }
                    }
                    .padding(.bottom)
                }
